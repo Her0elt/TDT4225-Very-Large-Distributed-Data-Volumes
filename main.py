c48d6abb7d7a040186933ceec2566f2528afe9fb
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-09-27 16:24:44 +0200

    fix loading and do more tasks

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ from database_util import DatabaseUtil
 def main():
     program = DatabaseUtil()
     try:
-        #program.setup()
-        #program.insert_users()
-        #program.insert_trajectory()
-        # program.task_2_1_count_tables_sizes()
-        # program.task_2_2_average_activities_per_user()
-        #program.task_2_3_top_20_users_with_most_activities()
+        # program.setup()
+        # program.insert_users()
+        # program.insert_trajectory()
+        
+        # Tasks
+        program.task_2_1_count_tables_sizes()
+        program.task_2_2_average_activities_per_user()
+        program.task_2_3_top_20_users_with_most_activities()
         program.task_2_4_users_whos_taken_taxi()
+        program.task_2_5_count_transportations()
+        program.task_2_6_a_year_with_most_activities()
+        program.task_2_6_b_year_with_longest_activities()
+        program.task_2_7_total_distance_walked_in_2008_by_112()
+        program.task_2_8_top_20_users_with_most_altitude_meters()
+        # program.task_2_9_invalid_activities_per_user()
+        program.task_2_10_users_tracked_forbidden_city()
         if program:
             program.connection.close_connection()
     except Exception as e:
--- util.py
+++ util.py
@@ -2,7 +2,7 @@ class Trajectory:
     
     def __init__(self, line: str):
         # 39.975818526,116.331600228,0,492.126286089239,40758.3839351852,2011-08-03,09:12:52
-        parts = line.split(',')
+        parts = line.strip().split(',')
         self.lat = parts[0]
         self.lon = parts[1]
         self.altitude = parts[3]
@@ -15,7 +15,7 @@ class LabelsActivity:
 
     def __init__(self, line: str):
         # 2008/12/06 10:30:11	2008/12/06 11:17:54	subway
-        parts = line.split(' ')
+        parts = line.split()
         self.start_date = f"{parts[0].replace('/', '-')} {parts[1]}"
         self.end_date = f"{parts[2].replace('/', '-')} {parts[3]}"
         self.transportation_mode = parts[4]
