0da5ebf99d7d6fba41a280c4762035b32e0d08c6
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-10-06 13:22:48 +0200

    Cleanup

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 class Trajectory:
     
     def __init__(self, line: str):
+        # Example line:
         # 39.975818526,116.331600228,0,492.126286089239,40758.3839351852,2011-08-03,09:12:52
         parts = line.strip().split(',')
         self.lat = parts[0]
@@ -14,6 +15,7 @@ class Trajectory:
 class LabelsActivity:
 
     def __init__(self, line: str):
+        # Example line:
         # 2008/12/06 10:30:11	2008/12/06 11:17:54	subway
         parts = line.split()
         self.start_date = f"{parts[0].replace('/', '-')} {parts[1]}"
